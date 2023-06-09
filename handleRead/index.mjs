f4fd8cce66f1a43735b55a05bf6ba498492667d7
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-09 18:20:17 -0500

    add to handle read index

--- handleRead/index.mjs
+++ handleRead/index.mjs
@@ -5,15 +5,21 @@ const schema = new dynamoose.Schema({
 "name": String
 });
 
+const people = dynamoose.model('people', schema)
+
 export const handler = async(event) => {
     console.log('this is the body', event.body);
     // TODO implement
     const response = {statusCode: null, body: null,};
 
 try {
-
+    let results = await people.scan().exec();
+    // console.log('results----', results);
+    response.body = JSON.stringify('watermelon');
+response.statusCode = 200;
 }catch(err){
     response.body = JSON.stringify(err.message);
+    response.statusCode = 500;
 }
 
 // original response
