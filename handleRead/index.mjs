aa883b31b57543b04a9af4b7542cdfc18fc6f55b
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-09 18:00:10 -0500

    edit index

--- handleRead/index.mjs
+++ handleRead/index.mjs
@@ -1,10 +1,25 @@
 import dynamoose from 'dynamoose';
 
+const schema = new dynamoose.Schema({
+"id": String,
+"name": String
+});
+
 export const handler = async(event) => {
+    console.log('this is the body', event.body);
     // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    const response = {statusCode: null, body: null,};
+
+try {
+
+}catch(err){
+    response.body = JSON.stringify(err.message);
+}
+
+// original response
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
     return response;
 };
